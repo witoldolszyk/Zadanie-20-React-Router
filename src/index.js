a042b6d575320416e52682148973485ec8acb8dc
Witold Olszyk <witoldolszyk@gmail.com>
2018-04-05 00:34:48 +0200

    add components and routes

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import React from 'react';
 import { render } from 'react-dom';
+import { Router, hashHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store from './store';
 import DevTools from './DevTools';
+import routes from './routes';
+
 
 render(
     <Provider store={store}>
-        <div>
-            <h1>Państwa-projekt</h1>
-            <DevTools />
-        </div>
+        <Router history={hashHistory} routes={routes} />
     </Provider>,
     document.getElementById('root')
 );
