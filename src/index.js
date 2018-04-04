16c3e672835d4de80082cb89b13130579e5bc812
Witold Olszyk <witoldolszyk@gmail.com>
2018-04-05 00:22:37 +0200

    add dev

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store';
+import DevTools from './DevTools';
 
 render(
     <Provider store={store}>
-        <h1>Państwa-projekt</h1>
+        <div>
+            <h1>Państwa-projekt</h1>
+            <DevTools />
+        </div>
     </Provider>,
     document.getElementById('root')
 );
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import { createStore } from 'redux';
 import reducers from '../reducers/index';
+import DevTools from '../DevTools';
 
-const store = createStore(reducers);
+const store = createStore(reducers, DevTools.instrument());
 
 export default store;
